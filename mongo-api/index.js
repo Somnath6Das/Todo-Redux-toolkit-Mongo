13465748688506b6465110618b60370d2360befa
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2023-02-14 18:39:00 +0530

    update

--- mongo-api/index.js
+++ mongo-api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { User } from "./models/user.js"
 
 const app = express();
@@ -22,12 +23,34 @@ app.post('/signup', async (req, res) => {
         }
         const user = await User.findOne({ email });
         if (user) {
-            return res.status(422).json({ error: "User already exist" });
+            return res.status(422).json({ error: "User already exist with same email!" });
         }
         const hashedPassword = await bcrypt.hash(password, 12);
         // save to mongodb
         await new User({ email, password: hashedPassword }).save();
         res.status(200).json({ message: "Signup success! Login now." })
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(422).json({ error: "Please add all the fields" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ error: "User does not exist with same email!" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+           const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+           res.status(201).json({token});
+        } else {
+            return res.status(401).json({ error: "User email and password does not match!" });
+        }
     } catch (err) {
         console.log(err);
     }
